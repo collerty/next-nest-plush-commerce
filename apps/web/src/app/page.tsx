f7cfaf5cf834dc3a419bfd1dc1116f089b19d842
unknown <nhlsviatoslav@gmail.com>
2024-11-02 00:04:41 +0100

    added NEXT_PUBLIC_API_URL variable

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,13 @@
 import {Card, CardContent,  CardFooter, CardHeader} from "@/components/ui/card";
 
 export default async function Home() {
-  const data = await fetch("http://localhost:4000/product");
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/product`);
   const products = await data.json();
   // console.log(products);
   return (
       <div className="w-full h-full flex flex-col">
         <div className="flex">
-          // eslint-disable-next-lin
+          {/* eslint-disable-next-line*/}
           {products.map((product:any) => {
             return (
                 <Card key={product.id}>
