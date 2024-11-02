95363bb930b7ab465c558f0263b52ca5208dbe1e
unknown <nhlsviatoslav@gmail.com>
2024-11-02 03:32:39 +0100

    test Railway Postgres db

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -16,16 +16,19 @@ import {ProductModule} from './product/product.module';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        ssl: {
-          rejectUnauthorized: false, // Consider enabling in production
-        },
-        synchronize: true, // Disable in production
+        url: configService.get<string>('DB_HOST'), // Use the full connection string
+        autoLoadEntities: true,
+        synchronize: true, // Consider changing to false in production
+        // host: configService.get<string>('DB_HOST'),
+        // port: configService.get<number>('DB_PORT'),
+        // username: configService.get<string>('DB_USER'),
+        // password: configService.get<string>('DB_PASSWORD'),
+        // database: configService.get<string>('DB_NAME'),
+        // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        // ssl: {
+        //   rejectUnauthorized: false, // Consider enabling in production
+        // },
+        // synchronize: true, // Disable in production
       }),
     }),
     ProductModule,
