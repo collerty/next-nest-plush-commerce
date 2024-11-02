7868c727426061f981419d5f0e078b73d2b4405c
unknown <nhlsviatoslav@gmail.com>
2024-11-02 03:19:12 +0100

    added PORT env variable to main.ts

--- apps/api/dist/main.js
+++ apps/api/dist/main.js
@@ -2,9 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const config_1 = require("@nestjs/config");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(4000);
+    const configService = app.get(config_1.ConfigService);
+    const PORT = configService.get('PORT');
+    await app.listen(PORT || 4000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ConfigModule, ConfigService} from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
+  await app.listen(PORT || 4000);
 }
+
 bootstrap();
