e2981ff94633333c1146610ccaaf4876978aec15
unknown <nhlsviatoslav@gmail.com>
2024-11-20 08:02:34 +0100

    added category crud

--- apps/api/src/categories/categories.service.ts
+++ apps/api/src/categories/categories.service.ts
@@ -1,26 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {CreateCategoryDto} from './dto/create-category.dto';
+import {UpdateCategoryDto} from './dto/update-category.dto';
+import {Repository} from "typeorm";
+import {Category} from "./entities/category.entity";
+import {InjectRepository} from "@nestjs/typeorm";
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+      @InjectRepository(Category)
+      private categoryRepository: Repository<Category>
+  ) {
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const category = this.categoryRepository.create(createCategoryDto);
+    return this.categoryRepository.save(category);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  findAll(): Promise<Category[]> {
+    return this.categoryRepository.find();
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async findOne(id: number): Promise<Category> {
+    const category = await this.categoryRepository.findOneBy({id});
+    if (!category) {
+      throw new NotFoundException(`Category with ID ${id} not found`);
+    }
+    return category;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async update(id: number, updateCategoryDto: UpdateCategoryDto): Promise<Category> {
+    const category = await this.findOne(id);
+    Object.assign(category, updateCategoryDto);
+
+    return this.categoryRepository.save(category);
+  }
+
+  async remove(id: number) {
+    const result = await this.categoryRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
   }
 }
--- apps/api/src/categories/dto/create-category.dto.ts
+++ apps/api/src/categories/dto/create-category.dto.ts
@@ -1,1 +1,9 @@
-export class CreateCategoryDto {}
+import {IsNotEmpty} from "class-validator";
+import {Product} from "../../products/entities/product.entity";
+
+export class CreateCategoryDto {
+  @IsNotEmpty()
+  name: string;
+
+  products?: Product[];
+}
