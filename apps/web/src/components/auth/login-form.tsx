61f07c754f1b30de745acf685a4428cd4b06147a
unknown <nhlsviatoslav@gmail.com>
2024-12-16 14:28:01 +0100

    added github login/register button

--- apps/web/src/components/auth/login-form.tsx
+++ apps/web/src/components/auth/login-form.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/card"
 import Link from "next/link";
 import {GoogleButton} from "@/components/auth/google-button";
+import {GithubButton} from "@/components/auth/github-button";
 
 export function LoginForm({
                             className,
@@ -28,18 +29,8 @@ export function LoginForm({
             <div>
               <div className="grid gap-6">
                 <div className="flex flex-col gap-4">
-                  <Button variant="outline" className="w-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                         stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                         className="lucide lucide-github">
-                      <path
-                          d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/>
-                      <path d="M9 18c-4.51 2-5-2-7-2"/>
-                    </svg>
-                    Login with Github
-                  </Button>
-                  <GoogleButton text={"Login with Google"} apiUrl={apiUrl} />
-
+                  <GithubButton text={"Login with Github"} apiUrl={apiUrl}/>
+                  <GoogleButton text={"Login with Google"} apiUrl={apiUrl}/>
                 </div>
                 <div className="text-center text-sm">
                   Don&apos;t have an account?{" "}
